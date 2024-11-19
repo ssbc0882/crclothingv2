bca5b33387504009bd204d72307918e748609f81
Bryan <bryan.heshiki@gmail.com>
2024-11-18 16:56:07 -0800

    store reducers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.scss";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { UserProvider } from "./contexts/user.context";
 import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
 
@@ -13,14 +14,14 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
+    <Provider store={store}>
+      <BrowserRouter>
         <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
         </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
